ff663c0e36c133a0131bb3dd73bf07a3241b997e
Shravani Raut <shravani.236327205@vcet.edu.in>
2026-06-01 11:58:59 +0530

    Middleware introduced

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -8,25 +8,8 @@ const imagekit = new ImageKit({
 });
 
 async function createPostController(req, res) {
-  console.log(req.body, req.file);
 
-  const token = req.cookies.token;
 
-  if (!token) {
-    return res.status(401).json({
-      message: "Token not provided, Unauthorised access",
-    });
-  }
-
-  let decoded = null;
-  
-  try{
-    decoded = jwt.verify(token, process.env.JWT_SECRET);
-  }catch(err){
-    return res.status(401).json({
-      message: "Unauthorised access"
-    })
-  }
 
   const file = await imagekit.files.upload({
     file: await toFile(Buffer.from(req.file.buffer), "file"),
@@ -38,7 +21,7 @@ async function createPostController(req, res) {
   const post = await postModel.create({
     caption: req.body.caption,
     image: file.url,
-    user: decoded.id,
+    user: req.user.id,
   });
 
   res.status(201).json({
@@ -47,6 +30,52 @@ async function createPostController(req, res) {
   });
 }
 
+async function requestPostController(req, res) {
+  
+
+  const userId = req.user.id
+
+  const post = await postModel.find({
+    user: userId
+  });
+  
+  res.status(200).json({
+    message: "Post fetched",
+    post
+  })
+}
+
+async function getPostDetails(req, res){
+  
+
+  const userId = req.user.id
+  const postId = req.params.postId
+  
+  const post = await postModel.findById(postId)
+
+  if(!post){
+    return res.status(404).json({
+      message: "Post Not Found"
+    })
+  }
+
+
+  const isValidUser = post.user.toString() === userId
+
+  if(!isValidUser){
+    return res.status(404).json({
+      message:"Forbidden Content"
+    })
+  }
+
+  return res.status(200).json({
+    message: "Post fetched successfully",
+    post
+  })
+}
+
 module.exports = {
   createPostController,
+  requestPostController,
+  getPostDetails
 };
--- src/routes/post.route.js
+++ src/routes/post.route.js
@@ -2,9 +2,14 @@ const express = require("express");
 const postController = require("../controllers/post.controller");
 const multer = require('multer')
 const upload = multer({storage: multer.memoryStorage()})
+const identifyUser = require('../middlewares/auth.middleware')
 
 const postRouter = express.Router();
 
-postRouter.post("/",upload.single("image"), postController.createPostController);
+postRouter.post("/",upload.single("image"), identifyUser, postController.createPostController);
+
+postRouter.get("/",  identifyUser,postController.requestPostController);
+
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetails);
 
 module.exports = postRouter;
